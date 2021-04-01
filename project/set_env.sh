6b6f1716ce3b37dbef32a1098031ab05ea689dbd
du2x <dudumonteiro@gmail.com>
2021-04-01 19:29:02 -0300

    monolith to microservices

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=cHuvinha123
+export POSTGRES_HOST=database-3.cct0mfp5za25.sa-east-1.rds.amazonaws.com
+export POSTGRES_DB=database-3
+export AWS_BUCKET=udagram-ex2
+export AWS_REGION=sa-east-1
+export AWS_PROFILE=998853712570
+export JWT_SECRET=sdlkjahjkfdskjlafdshfhdskajdfas32167321
 export URL=http://localhost:8100
---
+++ project/set_env_local.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=myuser
+export POSTGRES_PASSWORD=mypass
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=mydb
+export AWS_BUCKET=udagram-ex2
+export AWS_REGION=sa-east-1
+export AWS_PROFILE=998853712570
+export JWT_SECRET=sdlkjahjkfdskjlafdshfhdskajdfas32167321
+export URL=http://localhost:8100
